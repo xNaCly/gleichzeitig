d07103e9cd0b25174d93c3eddbcad5086b897b8a
xnacly <47723417+xNaCly@users.noreply.github.com>
2023-03-28 12:54:03 +0200

    refactor: allow printing of empty lines

--- util.go
+++ util.go
@@ -33,9 +33,6 @@ var wg sync.WaitGroup
 // prints not empty text with color to allow the user to separate the output of different commands.
 // index is used to select a color, therefore a maximum of 6 commands can be executed in parallel
 func commandPrint(index int, text string) {
-	if len(text) == 0 {
-		return
-	}
 	i := func() string {
 		if index < 9 {
 			return fmt.Sprintf("0%d", index)
